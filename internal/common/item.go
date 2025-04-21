22333f5838deea047da9b473ee912c016271d9cb
Brandon Bakker <brandon@bdmd.com.au>
2025-04-21 19:06:54 +1000

    mark as rquired

--- internal/common/item.go
+++ internal/common/item.go
@@ -2,11 +2,11 @@ package common
 
 // Item represents the structure of the JSON object
 type Item struct {
-	Title          string `json:"Title" jsonschema_description:"Title of the post"`
-	ID             string `json:"ID" jsonschema_description:"Post ID"`
-	Summary        string `json:"Summary" jsonschema_description:"Provide a summary of the post content"`
-	CommentSummary string `json:"Comment Summary" jsonschema_description:"Provide a summary and semtiment of the comments"`
-	Link           string `json:"Link" jsonschema_description:"A link to the post"`
-	Relevance      string `json:"Relevance" jsonschema_description:"Why is this relevant?"`
-	IsRelevant     bool   `json:"IsRelevant" jsonschema_description:"Should this be included?"`
+	Title          string `json:"Title" jsonschema_description:"Title of the post" jsonschema:"required"`
+	ID             string `json:"ID" jsonschema_description:"Post ID" jsonschema:"required"`
+	Summary        string `json:"Summary" jsonschema_description:"Provide a summary of the post content" jsonschema:"required"`
+	CommentSummary string `json:"Comment Summary" jsonschema_description:"Provide a summary and semtiment of the comments" jsonschema:"required"`
+	Link           string `json:"Link" jsonschema_description:"A link to the post" jsonschema:"required"`
+	Relevance      string `json:"Relevance" jsonschema_description:"Why is this relevant?" jsonschema:"required"`
+	IsRelevant     bool   `json:"IsRelevant" jsonschema_description:"Should this be included?" jsonschema:"required"`
 }
