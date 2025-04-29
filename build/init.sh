0714d1764fba424bfb4d44cc489721180091aba1
Brandon Bakker <brandon@bdmd.com.au>
2025-04-29 12:00:36 +1000

    Add debug option to skip cron setup in init script
    - Introduced a new environment variable `DEBUG_SKIP_CRON` to allow skipping the cron setup during initialization for debugging purposes.
    - Updated the `init.sh` script to conditionally run the main application directly if `DEBUG_SKIP_CRON` is set to true.
    - Added a corresponding field in the `Specification` struct to support the new debug option.

--- build/init.sh
+++ build/init.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
-sed -i "s/\[INSERT\]/$ANP_CRON_SCHEDULE/" /etc/cron.d/appcron
-printenv  >> /etc/environment
-
-cat /etc/cron.d/appcron | crontab -
 
 echo "Initialising AI News Processor"
-echo "Running at $ANP_CRON_SCHEDULE"
-crond -f -L "/dev/stdout"
+
+if [ "$DEBUG_SKIP_CRON" = "true" ]; then
+    echo "Debug mode: Skipping cron setup and running main directly"
+    /app/main
+else
+    sed -i "s/\[INSERT\]/$ANP_CRON_SCHEDULE/" /etc/cron.d/appcron
+    printenv  >> /etc/environment
+    
+    cat /etc/cron.d/appcron | crontab -
+    
+    echo "Running at $ANP_CRON_SCHEDULE"
+    crond -f -L "/dev/stdout"
+fi
